d4e954a847c26b687530a2853c76a0fc2e5ed5a7
Yonas Ketema <yonask.kitaw@gmail.com>
2023-01-24 19:14:43 +0300

    #412. Fizz Buzz

---
+++ leetCode/#412.FizzBuzz/FizzBuzz.ts
@@ -0,0 +1,15 @@
+function fizzBuzz(n: number): string[] {
+  let fizzBuzz: string[] = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      fizzBuzz.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      fizzBuzz.push("Fizz");
+    } else if (i % 5 === 0) {
+      fizzBuzz.push("Buzz");
+    } else {
+      fizzBuzz.push(`${i}`);
+    }
+  }
+  return fizzBuzz;
+}
