956c7873bc5d6646e483b649227d5bb9fb3f02a6
Yonas Ketema <yonask.kitaw@gmail.com>
2023-01-25 21:37:14 +0300

    #876.MiddleoftheLinkedList

--- leetCode/#876.MiddleoftheLinkedList/MiddleoftheLinkedList.ts
+++ leetCode/#876.MiddleoftheLinkedList/MiddleoftheLinkedList.ts
@@ -9,6 +9,18 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var middleNode = function(head) {
-    
+var middleNode = function (head) {
+  let currentNode = head;
+  let midNode = head;
+  let i = 1;
+  while (currentNode !== null) {
+    if (i % 2 !== 0 && currentNode.next !== null) {
+      midNode = midNode.next;
+    }
+
+    currentNode = currentNode.next;
+    i++;
+  }
+
+  return midNode;
 };
