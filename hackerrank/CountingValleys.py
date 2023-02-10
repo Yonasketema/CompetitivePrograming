575e965c31153fdd8a7c9522d4b54399bca4e788
Yonas Ketema <yonask.kitaw@gmail.com>
2023-02-10 21:10:06 +0300

    Counting Valleys.py

---
+++ hackerrank/CountingValleys.py
@@ -0,0 +1,17 @@
+ def countingValleys(steps, path):
+    
+    level = 0
+    valleys = 0
+    
+    for step in path:
+    
+        if step == 'U':
+            level+=1
+            
+            if level == 0:
+                valleys+=1
+                
+        else:
+            level-=1
+            
+    return valleys
